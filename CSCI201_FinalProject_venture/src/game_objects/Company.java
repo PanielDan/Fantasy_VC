970c678452672d15f8848f37a39d9b0bbfe50b2e
Aaron Schrock <aaronrschrock@Aarons-MacBook-Pro-2.local>
2016-11-13 13:18:43 -0800

    beginning on Company

--- CSCI201_FinalProject_venture/src/game_objects/Company.java
+++ CSCI201_FinalProject_venture/src/game_objects/Company.java
@@ -1,5 +1,29 @@
 package game_objects;
 
-public class Company {
+/**
+ * The {@code Company} class stores information about a company.
+ * Each company is matched with one of the trends form the MySQL database
+ * The Company will either belong to the Game or a specific user
+ *
+ * @author arschroc
+ * 
+ */
 
+public class Company {
+	
+	private String image, name, description;
+	private int startingPrice, askingPrice, currentWorth, tierLevel;
+	
+	public Company(String image, String name, String description, int startingPrice, int currentWorth, int tierLevel) {
+		this.image = image;
+		this.name = name;
+		this.description = description;
+		this.startingPrice = startingPrice;
+		askingPrice = startingPrice;
+		this.currentWorth = currentWorth;
+		this.tierLevel = tierLevel;
+	}
+	
+	
+	
 }
