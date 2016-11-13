870dcea2342961f35df575f1b9a3603327b039e2
Aaron Schrock <aaronrschrock@Aarons-MacBook-Pro-2.local>
2016-11-13 14:02:32 -0800

    beginning Game

---
+++ CSCI201_FinalProject_venture/src/game_objects/Game.java
@@ -0,0 +1,13 @@
+package game_objects;
+
+/**
+ * Maintains all of the current players inside of the instance of the game
+ * Has a list of all the companies that are still available
+ * Also keeps track of the game state
+ *
+ * @author arschroc
+ * 
+ */
+public class Game {
+	
+}
