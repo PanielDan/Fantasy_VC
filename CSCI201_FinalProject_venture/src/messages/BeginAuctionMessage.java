5a3ef569bc5f3886b51b2f57b6ecadf0e9331892
Alan Coon <alancoon@usc.edu>
2016-11-14 21:05:43 -0800

    Added BeginAuctionMessage and BeginTimelapseMessage.

---
+++ CSCI201_FinalProject_venture/src/messages/BeginAuctionMessage.java
@@ -0,0 +1,11 @@
+package messages;
+
+public class BeginAuctionMessage extends Message {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public BeginAuctionMessage() { } 
+}
---
+++ CSCI201_FinalProject_venture/src/messages/BeginTimelapseMessage.java
@@ -0,0 +1,12 @@
+package messages;
+
+
+public class BeginTimelapseMessage extends Message {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public BeginTimelapseMessage() { }
+}
