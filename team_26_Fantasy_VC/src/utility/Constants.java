9a0457555ddc1660c523ba5b910fd941450ccf56
Alan Coon <alancoon@usc.edu>
2016-11-15 00:39:47 -0800

    Added more event blurbs to Constants.

--- team_26_Fantasy_VC/src/utility/Constants.java
+++ team_26_Fantasy_VC/src/utility/Constants.java
@@ -10,11 +10,23 @@ public class Constants {
 		{ "'s C.E.O. did something cool.",
 		  " had a great quarter!",
 		  "'s new product was a massive success!",
-		  " streamlined production costs!" };
+		  " streamlined production costs!",
+		  " synergized their backwards overflow!",
+		  " collaboratively synthesized goal-oriented human capital!",
+		  " interactively developed client-based expertise!",
+		  " proactively harness high-payoff resources!",
+		  " rapidiously engineered end-to-end relationships!",
+		  " globally conceptualized turnkey scenarios!",
+		  " assertively recaptiualized value-added strategic theme niches!" 
+		  };
 	
 	public static final String negativeEvents[] = 
 		{ "'s C.E.O. caught in embezzlement scandal.",
 		  "'s product was a failure.", 
 		  " is having a hard quarter.", 
-		  "'s office building burned down!! Oh no!" };
+		  "'s office building burned down!! Oh no!",
+		  " condemned by U.S. Government.", 
+		  " caught evading taxes!",
+		  "'s C.E.O. gambled away company money!"
+		  };
 }
