7b3168bf93ff1d3b2d669efa503fe277903fb85a
Alan Coon <alancoon@usc.edu>
2016-11-14 21:25:30 -0800

    Added BeginQuarterlyMessage and EndGameMessage.

---
+++ CSCI201_FinalProject_venture/src/messages/BeginQuarterlyMessage.java
@@ -0,0 +1,11 @@
+package messages;
+
+public class BeginQuarterlyMessage extends Message {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public BeginQuarterlyMessage() { }
+}
---
+++ CSCI201_FinalProject_venture/src/messages/EndGameMessage.java
@@ -0,0 +1,12 @@
+package messages;
+
+public class EndGameMessage extends Message {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public EndGameMessage() { }
+	
+}
