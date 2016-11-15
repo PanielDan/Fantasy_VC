ecd903183024b267aed24ad63688ade755734e5b
Alan Coon <alancoon@usc.edu>
2016-11-15 01:22:30 -0800

    Cleaned and finalized updateCurrentWorth() in Company.java

--- team_26_Fantasy_VC/src/gameplay/Company.java
+++ team_26_Fantasy_VC/src/gameplay/Company.java
@@ -2,6 +2,8 @@ package gameplay;
 
 import java.util.Random;
 
+import utility.Constants;
+
 /**
  * The {@code Company} class stores information about a company.
  * Each company is matched with one of the trends form the MySQL database
@@ -58,36 +60,48 @@ public class Company {
 	}
 	
 	//SETTER functions
+	
+	/**
+	 * 
+	 * @return A {@code String} of text that we can add to the notifications
+	 * box on every {@code Client}'s {@code TimelapseGUI}.  A random
+	 * outcome will determine if a special event occurs.  If there is 
+	 * no special event, the returned text will be an empty {@code String}.
+	 * Otherwise it will describe a good or bad event, also determined
+	 * by random outcome.
+	 */
 	public synchronized String updateCurrentWorth() {
 		Random rand = new Random();
 		int change = Math.abs(rand.nextInt(delta));
 		boolean positive = rand.nextBoolean();
 		boolean specialEvent = rand.nextBoolean();
-		
-		if(positive) {
-			if(specialEvent) {
-				change*=3;
-				currentWorth += change;
-				int index = rand.nextInt(Constants.positiveEvents.length - 1);
-				return Constants.positiveEvents[index];
-			}
-			
-			currentWorth += change;
-			return null;
-		}
-		else {
-			if(specialEvent) {
-				change *= 3;
-				currentWorth -= change;
-				int index = rand.nextInt(Constants.negativeEvents.length - 1);
-				return Constants.negativeEvents[index];
-			}
-			
-			currentWorth -= change;
-			return null;
-		}
-		
+		String text = "";
 
+		// First we check if a special event has occurred. 
+		if (specialEvent) { 
+			// We will either boost or decrease this turn's delta by
+			// a factor of between 3 and 6.
+			int modifier = Math.abs(rand.nextInt(3)) + 3;
+			int index;
+			if (positive) { 
+				change *= modifier;  // Boost the change by the modifier.
+				index = rand.nextInt(Constants.positiveEvents.length - 1); // Randomly pick an event text.
+				text = Constants.positiveEvents[index];  // Grab the event text.
+			} else {
+				change *= -modifier;
+				index = rand.nextInt(Constants.negativeEvents.length - 1);
+				text = Constants.negativeEvents[index];
+			}
+		} else {
+			// If no special event has occurred, we just modify the 
+			// change based on whether positive came out true or not.
+			change = positive ? change : -change;
+		}
+		
+		// At the end we just add the change to the currentWorth,
+		// and we return the text.  
+		currentWorth += change;
+		return text;
 	}
 	
 	public void updateAskingPrice(int askingPrice) {
