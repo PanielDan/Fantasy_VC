3e06dd975f691dab9acc777ef3fbbb3d5df709c7
Alan Coon <alancoon@usc.edu>
2016-11-15 01:56:09 -0800

    Hack and slashing at Server and ServerClientCommunicator.  Created PortBoundException for when a player tries to begin a game on a port that is already bound.

--- team_26_Fantasy_VC/src/server/Server.java
+++ team_26_Fantasy_VC/src/server/Server.java
@@ -1,5 +1,16 @@
 package server;
 
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Random;
+import java.util.Vector;
+
+import exceptions.PortBoundException;
+import messages.Message;
+
 /**
  * The {@code Server} is a {@code Thread} that
  * handles connects and handles messages from 
@@ -9,33 +20,16 @@ package server;
  *
  */
 public class Server extends Thread {
-	private StartWindowGUI hostGUI;
-	private MainGUI mGUI;
-	private Vector<JeopardyServerThread> serverThreads;
+	private Vector<ServerClientCommunicator> serverClientCommunicators;
 	private ServerSocket ss;
 	private int numberOfPlayers;
-	private GameFile gameFile;
-	private Vector<String> teamNames;
-	
-	private QuestionState currentQuestionState;
-	private int numberOfBets;
-	private int numberOfAnswers;
-
-
-	private boolean allConnected;
 
 	
-	public JeopardyServer(StartWindowGUI hostGUI, int port, GameFile gameFile) throws PortBoundException {
+	public Server(int port) throws PortBoundException {
 		this.ss = null;
-		this.hostGUI = hostGUI;
 		this.numberOfPlayers = hostGUI.getSlider().getValue();
-		this.serverThreads = new Vector<JeopardyServerThread>();
-		this.gameFile = gameFile;
+		this.serverThreads = new Vector<ServerClientCommunicator>();
 		this.teamNames = new Vector<String>();
-		this.allConnected = false;
-		this.allNamesReceived = false;
-		this.allBetsReceived = false;
-		this.allAnswersReceived = false;
 		
 		try {
 			ss = new ServerSocket(port);
@@ -47,12 +41,12 @@ public class Server extends Thread {
 			hostGUI.reenableAfterDisconnection();
 			this.kill();
 		} catch (IOException ioe) {
-			System.out.println("IOException in JeopardyServer::JeopardyServer(): " + ioe.getMessage());
+			System.out.println("IOException in Server::Server(): " + ioe.getMessage());
 			ioe.printStackTrace();
 			try { 
 				if (ss != null) { ss.close(); }
 			} catch (IOException iioe) { 
-				System.out.println("IOException closing ServerSocket in JeopardyServer(): " + iioe.getLocalizedMessage());
+				System.out.println("IOException closing ServerSocket in Server(): " + iioe.getLocalizedMessage());
 				iioe.printStackTrace();
 			}
 		} 
@@ -69,7 +63,7 @@ public class Server extends Thread {
 					Thread.yield();
 					Socket s = ss.accept();
 					System.out.println("Connection from " + s.getInetAddress());
-					JeopardyServerThread st = new JeopardyServerThread(s, this, hostGUI.getUser());
+					ServerClientCommunicator scc = new ServerClientCommunicator(s, this, hostGUI.getUser());
 					serverThreads.add(st);
 					hostGUI.incrementPlayers();
 					allClientsUpdateLobbyStatus(hostGUI.getPlayers() + "/" + numberOfPlayers + " players joined...");
@@ -83,7 +77,6 @@ public class Server extends Thread {
 						HostQuitInitMessage message = new HostQuitInitMessage(teamNames.get(0));
 						this.sendHostDroppedMessage(message);
 						for (int i = 0; i < 20; i++) Thread.yield();
-	//					this.kill();
 						return;
 					}
 				}
@@ -97,51 +90,26 @@ public class Server extends Thread {
 						this.allNamesReceived = true;
 					}
 				} catch (InterruptedException ie) {
-					System.out.println("InterruptedException in JeopardyServer::run(): " + ie.getMessage());
+					System.out.println("InterruptedException in Server::run(): " + ie.getMessage());
 					ie.printStackTrace();
 				}
 			}
-			
-			
-			/* Inform the BuzzerActionListener of our team names. */
-			BuzzerActionListener.setTeamNames(teamNames);
-			
-			/* Send all clients a copy of the official team list! */
-			distributeTeamList();
-
-			/* Send all clients to the MainGUI. */
-			allClientsToMainGUI();
-			
-//			while (!this.allBetsReceived) { 
-//				Thread.yield();
-//				if (numberOfBets == numberOfPlayers) { this.allBetsReceived = true; }
-//			}
-			
-			/* Reveal answer to players. */
-//			RevealFinalQuestionMessage message = new RevealFinalQuestionMessage();
-//			sendMessageToAllClients(message);
-			
-			
-//			while (!this.allAnswersReceived) { 
-//				Thread.yield();
-//				if (numberOfAnswers == numberOfPlayers) { allAnswersReceived = true; }
-//			}
 		} catch (IOException ioe) {
-			System.out.println("IOException in JeopardyServer::run(): " + ioe.getMessage());
+			System.out.println("IOException in Server::run(): " + ioe.getMessage());
 			ioe.printStackTrace();
 		} finally {
 			if (ss != null) {
 				try {
 					ss.close();
 				} catch (IOException ioe) {
-					System.out.println("IOException in JeopardyServer::run(), closing ServerSocket: " + ioe.getMessage());
+					System.out.println("IOException in Server::run(), closing ServerSocket: " + ioe.getMessage());
 					ioe.printStackTrace();
 				}
 			}
 		}
 	}
 
-	public void removePlayerFromLobby(String teamName, JeopardyServerThread thread) { 
+	public void removePlayerFromLobby(String teamName, ServerClientCommunicator thread) { 
 		teamNames.remove(teamName);
 		serverThreads.remove(thread);
 		hostGUI.decrementPlayers();
@@ -149,8 +117,8 @@ public class Server extends Thread {
 	}
 	
 	public void allClientsUpdateLobbyStatus(String message) {
-		for (JeopardyServerThread thread : serverThreads) {
-			thread.sendMessage(new JeopardyMessage(0, message));
+		for (ServerClientCommunicator thread : serverThreads) {
+			thread.sendMessage(new Message(0, message));
 		}
 	}
 
@@ -169,7 +137,7 @@ public class Server extends Thread {
 		int numberOfQuestions = hostGUI.getQuickPlay() ? 5 : 25;
 		Random rand = new Random();
 		int firstTurn = Math.abs(rand.nextInt()) % numberOfPlayers;
-		for (JeopardyServerThread st : serverThreads) {
+		for (ServerClientCommunicator st : serverThreads) {
 			StartMainGUIMessage message = new StartMainGUIMessage();
 			message.setTeamNames(teamNames);
 			message.setNumberOfQuestions(numberOfQuestions);
@@ -181,71 +149,27 @@ public class Server extends Thread {
 
 	public void sendExitMessage(ClientQuitGameMessage message) {
 		String name = message.getTeamName();
-		for (JeopardyServerThread st : serverThreads) {
+		for (ServerClientCommunicator st : serverThreads) {
 			String threadTeamName = st.getTeamName();
 			if (!threadTeamName.equals(name)) {
 				st.sendMessage(message);
 			}
 		}
 	}
-	
-//	public void sendExitMessage(HostQuitInitMessage message) {
-//		String name = message.getTeamName();
-//		for (JeopardyServerThread st: serverThreads) { 
-//			String threadTeamName = st.getTeamName();
-//			System.out.println(name + " ? " + threadTeamName);
-//			if (!threadTeamName.equals(name)) {
-//				st.sendMessage(message);
-//			}
-// 		}
-//	}
-	
-	public void sendHostDroppedMessage(HostQuitInitMessage message) { 
-		for (JeopardyServerThread st : serverThreads) { 
-			System.out.println(st.getTeamName() + " !!! ");
+
+	public synchronized void sendMessageToAllClients(Message message) {
+		for (ServerClientCommunicator st : serverClientCommunicators) {
 			st.sendMessage(message);
 		}
-	}
-	
-	public synchronized void sendMessageToAllClients(JeopardyMessage message) {
-		for (JeopardyServerThread st : serverThreads) {
-			st.sendMessage(message);
-		}
-	}
-
-	public void sendTeamName(String message) {
-		teamNames.add(message);
-	}
-
-	public void distributeTeamList() {
-		DistributeTeamListMessage message = new DistributeTeamListMessage("Distributing team list.", teamNames);
-		for (JeopardyServerThread st : serverThreads) {
-			st.sendMessage(message);
-		}
-	}
-
-	public void resetServer() {
-		this.numberOfBets = 0;
-		this.numberOfAnswers = 0;
-		this.currentQuestionState = null;
-		this.allAnswersReceived = false;
-		this.allBetsReceived = false;
 	}
 	
 	public void kill() {
 		try {
 			if (ss != null) { ss.close(); }
 		} catch (IOException ioe) {
-			System.out.println("IOException in JeopardyServer::kill(): " + ioe.getLocalizedMessage());
+			System.out.println("IOException in Server::kill(): " + ioe.getLocalizedMessage());
 			ioe.printStackTrace();
 		}
 	}
 
-	public void incrementNumberOfBets() {
-		this.numberOfBets++;
-	}
-	
-	public void setCurrentQuestionState(QuestionState currentQuestionState) {
-		this.currentQuestionState = currentQuestionState;
-	}
 }
--- team_26_Fantasy_VC/src/server/ServerClientCommunicator.java
+++ team_26_Fantasy_VC/src/server/ServerClientCommunicator.java
@@ -1,52 +1,43 @@
-package game_logic;
+package server;
 
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-import messages.BetPlacedMessage;
-import messages.BuzzMessage;
-import messages.ClientQuitGameMessage;
-import messages.ClientQuitInitMessage;
-import messages.CorrectAnswerMessage;
-import messages.FinalAnswerSubmittedMessage;
-import messages.IncorrectAnswerMessage;
-import messages.JeopardyMessage;
-import messages.NoRightAnswerMessage;
-import messages.QuestionEnteredMessage;
-import messages.RevealFinalQuestionMessage;
-import messages.SecondChanceMessage;
-import messages.VoteMessage;
+import gameplay.User;
+import messages.ChatMessage;
+import messages.Message;
+
 
 public class ServerClientCommunicator extends Thread {
 
 	private ObjectInputStream ois;
 	private ObjectOutputStream oos;
-	private JeopardyServer js;
-	private String teamName;
+	private Server server;
+	private String firmName;
 	private User user;
 
-	public ServerClientCommunicator(Socket s, JeopardyServer js, User user) {
+	public ServerClientCommunicator(Socket s, Server server, User user) {
 		try {
-			this.js = js;
+			this.server = server;
 			this.user = user;
 			oos = new ObjectOutputStream(s.getOutputStream());
 			ois = new ObjectInputStream(s.getInputStream());
 			this.start();
 		} catch (IOException ioe) {
-			System.out.println("ioe: " + ioe.getMessage());
+			System.out.println("IOException in ServerClientCommunicator: " + ioe.getMessage());
+			ioe.printStackTrace();
 		}
 	}
 
-	public void sendMessage(JeopardyMessage message) {
+	public void sendMessage(Message message) {
 		try {
 			oos.writeObject(message);
 			oos.flush();
-//			oos.reset();
+			oos.reset();
 		} catch (IOException ioe) {
-			System.out.println("IOException in JeopardyServerThread::sendMessage(): " + ioe.getMessage());
+			System.out.println("IOException in ServerClientCommunicator::sendMessage(): " + ioe.getMessage());
 			ioe.printStackTrace();
 		}
 	}
@@ -54,121 +45,48 @@ public class ServerClientCommunicator extends Thread {
 	public void run() {
 		while (true) { 
 			try {
-				//				JeopardyMessage message = (JeopardyMessage) ois.readObject();
 				Object o = ois.readObject();
-				if (o != null) { 
-					JeopardyMessage message = (JeopardyMessage) o;
-					if (message != null) { 
-						System.out.println("ServerThread Receiving: " + message.getType() + " | " + message.getMessage());
-
-						if (message.getType() == 1) {
-							this.setTeamName(message.getMessage());
-							js.sendTeamName(message.getMessage());
-						} else if (message.getType() == 2) {
-							ClientQuitInitMessage _message = (ClientQuitInitMessage) message;
-							js.removePlayerFromLobby(_message.getTeamName(), this);
-						} else if (message.getType() == 4) {
-							/* Someone exit. */
-							ClientQuitGameMessage _message = (ClientQuitGameMessage) message;
-							js.sendExitMessage(_message);
-						} else if (message.getType() == 199) { 
-							js.resetServer();
-							js.sendMessageToAllClients(message);
-						} else if (message.getType() == 200) {
-							/* Entered a question. */
-							QuestionEnteredMessage _message = (QuestionEnteredMessage) message;
-							js.setCurrentQuestionState(_message.currentQuestionState);
-							js.sendMessageToAllClients(message);
-						} else if (message.getType() == 210) { 
-							/* Correct answer. */
-							CorrectAnswerMessage _message = (CorrectAnswerMessage) message;
-							System.out.println(_message.getMessage());
-							js.sendMessageToAllClients(message);
-						} else if (message.getType() == 211) {
-							/* Incorrect answer. */
-							IncorrectAnswerMessage _message = (IncorrectAnswerMessage) message;
-							System.out.println(_message.getMessage());
-							js.sendMessageToAllClients(message);
-						} else if (message.getType() == 212) { 
-							/* Second chance. */
-							SecondChanceMessage _message = (SecondChanceMessage) message;
-							System.out.println(_message.getMessage());
-							js.sendMessageToAllClients(message);
-						} else if (message.getType() == 280) { 
-							/* Buzzed in.*/
-							BuzzMessage _message = (BuzzMessage) message;
-							System.out.println(_message.getMessage());
-							js.sendMessageToAllClients(message);
-						} else if (message.getType() == 290) {
-							/* Nobody got it right. */
-							/* DEPRECATED? */
-							NoRightAnswerMessage _message = (NoRightAnswerMessage) message;
-							System.out.println("Answer " + _message.getAnswer());
-							System.out.println("Message " + _message.getMessage());
-							js.sendMessageToAllClients(_message);
-						} else if (message.getType() == 291) {
-							/* NoRightAnswerMessage version 2. */
-							/* AllWrongMessage. */
-							js.sendMessageToAllClients(message);
-						} else if (message.getType() == 351) {
-							/* Bid placed. */
-							BetPlacedMessage _message = (BetPlacedMessage) message;
-							System.out.println(_message.getMessage());
-							js.sendMessageToAllClients(_message);
-						} else if (message.getType() == 352) {
-							/* Final Jeopardy answer submitted. */
-							FinalAnswerSubmittedMessage _message = (FinalAnswerSubmittedMessage) message;
-							System.out.println(_message.getMessage());
-							js.sendMessageToAllClients(_message);
-						} else if (message.getType() == 380) {
-							/* Reveal Final Jeopardy question to all players. **/
-							RevealFinalQuestionMessage _message = (RevealFinalQuestionMessage) message;
-							System.out.println(_message.getMessage());
-							js.sendMessageToAllClients(_message);
-						} else if (message.getType() == 450) {
-							/* Someone voted. */
-							/* Named after EE 450. */
-							VoteMessage _message = (VoteMessage) message;
-							System.out.println(_message.getMessage());
-							js.sendMessageToAllClients(_message);
-						} else if (message.getType() == 455) {
-							/* Update. */
-							System.out.println(message.getMessage());
-							js.sendMessageToAllClients(message);
-						} else {
-							System.out.println("Slow down there cowboy, that code doesn't exist.");
-						}
-					}
+				/* This is a BAD IMPLEMENTATION. TODO CHANGE THIS. */
+				if (o instanceof ChatMessage) {
+					server.sendMessageToAllClients((ChatMessage) o);
 				}
-			} catch (EOFException eofe) { 
-				System.out.println("HELLO THIS IS EOFEXCEPTION: " + eofe.getLocalizedMessage());
-				return;
 			} catch (ClassNotFoundException cnfe) {
-				System.out.println("ClassNotFoundException in JeopardyServerThread::run(): " + cnfe.getLocalizedMessage());
+				System.out.println("ClassNotFoundException in ServerClientCommunicator::run(): " + cnfe.getLocalizedMessage());
 				cnfe.printStackTrace();
 			} catch (IOException ioe) {
-				System.out.println("IOException in JeopardyServerThread::run(): " + ioe.getLocalizedMessage());
+				System.out.println("IOException in ServerClientCommunicator::run(): " + ioe.getLocalizedMessage());
 				ioe.printStackTrace();
 				return;
 			}
 		}
 	}
 
-	public void kill() {
+	public void closeObjectStream() {
 		try { 
 			if (ois != null) { ois.close(); }
 			if (oos != null) { oos.close(); }
 		} catch (IOException ioe) {
+			System.out.println("IOException in ServerClientCommunicator::closeObjectStream(): " + ioe.getLocalizedMessage());
 			return;
 		} 
 	}
 
+	/* Getters. */
 	public String getTeamName() {
-		return teamName;
+		return firmName;
+	}
+	
+	public User getUser() { 
+		return user;
 	}
 
-	public void setTeamName(String name) {
-		this.teamName = name;
+	/* Setters. */
+	public void setTeamName(String firmName) {
+		this.firmName = firmName;
+	}
+	
+	public void setUser(User user) { 
+		this.user = user;
 	}
 }
 
