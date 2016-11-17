333240cc60d63ecb693e291d06409b9a0f8b8787
Alan Coon <alancoon@usc.edu>
2016-11-16 20:44:51 -0800

    Patching server.

--- team_26_Fantasy_VC/venture.sql
+++ team_26_Fantasy_VC/venture.sql
@@ -9,6 +9,7 @@ create table Users (
 	userID 		int(11) 	primary key not null auto_increment,
 	username 	varchar(30) not null,
     passcode 	varchar(30) not null,
+    biography	varchar(144) not null,
     gamesPlayed int(11) 	not null default 0,
     gamesWon 	int(11) 	not null default 0,
     totalProfit bigint	 	not null default 0
