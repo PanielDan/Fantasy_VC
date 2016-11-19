7cfe4340e87f65328d2e0841d4db0a9d4fa8f6be
Timothy Lew <lewt@usc.edu>
2016-11-18 22:33:54 -0800

    pushing changes

--- team_26_Fantasy_VC/src/guis/CreateGameGUI.java
+++ team_26_Fantasy_VC/src/guis/CreateGameGUI.java
@@ -7,6 +7,8 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -16,6 +18,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
+import client.Client;
 import messages.CreateGameMessage;
 import utility.AppearanceConstants;
 
@@ -130,6 +133,27 @@ public class CreateGameGUI extends JFrame{
 	
 	private void addEvents() {
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		lobbyName.addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) { }
+
+			@Override
+			public void keyPressed(KeyEvent e) { }
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					if(!lobbyName.getText().equals("")) {
+						CreateGameMessage cgm = new CreateGameMessage(lobbyName.getText(), size.getItemAt(size.getSelectedIndex()), ip.gameFrame.user);
+						ip.gameFrame.getClient().sendMessage(cgm);
+						dispose();
+					}
+					else {
+						warningLabel.setText("Please enter a lobby name.");
+					}
+				}
+			}
+		});
 		createButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
 				if(!lobbyName.getText().equals("")) {
