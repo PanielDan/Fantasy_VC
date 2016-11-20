bf5259865081bdf27fd5859aa82da0273efc267f
Jeffrey Chen <chen305@usc.edu>
2016-11-20 01:16:08 -0800

    fixed my own mistake

--- team_26_Fantasy_VC/src/client/Client.java
+++ team_26_Fantasy_VC/src/client/Client.java
@@ -109,53 +109,59 @@ public class Client extends Thread {
 					}
 				}
 				else if(m instanceof ChatMessage) {
-					ChatMessage cm = (ChatMessage)m;
-					try{
-						if(!cm.getUsername().equals(gameFrame.user.getUsername())){
-							//System.out.println(cm.getUsername() + gameFrame.user.getUsername());
-							File f = new File("resources/chatSound.wav");
-							AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(f);
-							    Clip clip = AudioSystem.getClip();
-							    clip.open(audioInputStream);
-							    clip.start();
+					if(!(gameFrame.getCurrentPanel() instanceof IntroPanel)) {
+						ChatMessage cm = (ChatMessage)m;
+						try{
+							if(!cm.getUsername().equals(gameFrame.user.getUsername())){
+								//System.out.println(cm.getUsername() + gameFrame.user.getUsername());
+								File f = new File("resources/chatSound.wav");
+								AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(f);
+								    Clip clip = AudioSystem.getClip();
+								    clip.open(audioInputStream);
+								    clip.start();
+							}
+						} catch(Exception e) {
+							e.printStackTrace();
+						} finally {
+							gameFrame.getChatPanel().addChat(cm.getUsername(), cm.getMessage());
 						}
-					} catch(Exception e) {
-						e.printStackTrace();
-					} finally {
-						gameFrame.getChatPanel().addChat(cm.getUsername(), cm.getMessage());
 					}
 				}
 				else if (m instanceof LobbyPlayerReadyMessage) {
-					System.out.println("lprm");
-					LobbyPlayerReadyMessage lprm = (LobbyPlayerReadyMessage)m;
-					Vector<LobbyUserPanel> lup = ((LobbyPanel)gameFrame.getCurrentPanel()).getLobbyPanels();
-					for (LobbyUserPanel user : lup){
-						if (user.getUsername().equals(lprm.getUsername())){
-							System.out.println("set user");
-							user.setFirmName(lprm.getTeamName());
-							user.setReady();
+					if(gameFrame.getCurrentPanel() instanceof LobbyPanel){
+						System.out.println("lprm");
+						LobbyPlayerReadyMessage lprm = (LobbyPlayerReadyMessage)m;
+						Vector<LobbyUserPanel> lup = ((LobbyPanel)gameFrame.getCurrentPanel()).getLobbyPanels();
+						for (LobbyUserPanel user : lup){
+							if (user.getUsername().equals(lprm.getUsername())){
+								System.out.println("set user");
+								user.setFirmName(lprm.getTeamName());
+								user.setReady();
+							}
 						}
-					}
-					for (User user : users) {
-						if(user.getUsername().equals(lprm.getUsername())) {
-							user.setCompanyName(lprm.getTeamName());
+						for (User user : users) {
+							if(user.getUsername().equals(lprm.getUsername())) {
+								user.setCompanyName(lprm.getTeamName());
+							}
+							user.createIcon();
 						}
-						user.createIcon();
 					}
 				}
 				else if (m instanceof ReadyGameMessage) {
-					System.out.println("ready game");
-					atl = new AuctionTeamList(this, gameFrame); 
-					gameFrame.changePanel(atl);
-					System.out.println(user.getUsername() + " " + users.get(0).getUsername());
-					if(user.getUsername().equals(users.get(0).getUsername())) {
-						System.out.println("sending message");
-						sendMessage(new StartTimerMessage());
+					if(gameFrame.getCurrentPanel() instanceof LobbyPanel){
+						System.out.println("ready game");
+						atl = new AuctionTeamList(this, gameFrame); 
+						gameFrame.changePanel(atl);
+						System.out.println(user.getUsername() + " " + users.get(0).getUsername());
+						if(user.getUsername().equals(users.get(0).getUsername())) {
+							System.out.println("sending message");
+							sendMessage(new StartTimerMessage());
+						}
 					}
 				}
 				else if (m instanceof Game) {
 					if(gameFrame.getCurrentPanel() instanceof LobbyPanel) {
-						
+						((LobbyPanel) gameFrame.getCurrentPanel()).setWaitingText(-1);
 					}
 					System.out.println("new game " + ((Game)m).getCurrentQuarter());
 					gameFrame.setGame((Game)m);
@@ -173,15 +179,17 @@ public class Client extends Thread {
 					}
 				}
 				else if (m instanceof BeginAuctionBidMessage) { 
-					System.out.println("bidding now");
-					BeginAuctionBidMessage babm = (BeginAuctionBidMessage) m;
-					Company company = babm.getCompany();
-					company.createIcon();
-					AuctionBidScreen abs = new AuctionBidScreen(gameFrame, company);
-					abs.updateBet(babm.getCompanyName(), company.getStartingPrice());
-					gameFrame.changePanel(abs);
-					atl.removeRow(babm.getSelected());
-					atl.nextPlayer();
+					if(gameFrame.getCurrentPanel() instanceof AuctionTeamList){
+						System.out.println("bidding now");
+						BeginAuctionBidMessage babm = (BeginAuctionBidMessage) m;
+						Company company = babm.getCompany();
+						company.createIcon();
+						AuctionBidScreen abs = new AuctionBidScreen(gameFrame, company);
+						abs.updateBet(babm.getCompanyName(), company.getStartingPrice());
+						gameFrame.changePanel(abs);
+						atl.removeRow(babm.getSelected());
+						atl.nextPlayer();
+					}
 				}
 				else if (m instanceof AuctionBidUpdateMessage) {
 					if (gameFrame.getCurrentPanel() instanceof AuctionBidScreen){
@@ -191,6 +199,7 @@ public class Client extends Thread {
 				}
 				else if (m instanceof TimerDone) {
 					if (gameFrame.getCurrentPanel() instanceof AuctionBidScreen) {
+						System.out.println("ABS " + System.currentTimeMillis());
 						AuctionBidScreen auctionBidScreen = (AuctionBidScreen) gameFrame.getCurrentPanel();
 						auctionBidScreen.company.setCurrentWorth(auctionBidScreen.bidMin);
 						for(User u : users) {
@@ -217,11 +226,13 @@ public class Client extends Thread {
 						}
 					}
 					else if (gameFrame.getCurrentPanel() instanceof AuctionTeamList){
+						System.out.println("ATL " + System.currentTimeMillis());
 						if (user.getUsername().equals(atl.getCurrent())) {
 							atl.networkBidButtonAction();
 						}
 					}
 					else if (gameFrame.getCurrentPanel() instanceof QuarterlyGUI){
+						System.out.println("QGUI " + System.currentTimeMillis());
 						qgui.networkReadyFunctionality();
 					}
 				}
@@ -230,20 +241,17 @@ public class Client extends Thread {
 					if (gameFrame.getCurrentPanel() instanceof AuctionBidScreen) {
 						AuctionBidScreen auctionBidScreen = (AuctionBidScreen) gameFrame.getCurrentPanel();
 						auctionBidScreen.updateTimer(ttm.getDisplay());
-						if(ttm.getDisplay().equals("00:00")) {
-							sendMessage(new TimerDone());
-						}
 					}
 					else if (gameFrame.getCurrentPanel() instanceof AuctionTeamList) {
 						atl.updateTimer(ttm.getDisplay());
-						if(ttm.getDisplay().equals("00:00")){
-							sendMessage(new TimerDone());
-						}
 					}
 					else if(gameFrame.getCurrentPanel() instanceof QuarterlyGUI) {
 						qgui.updateTimer(ttm.getDisplay());
-						if(ttm.getDisplay().equals("00:00")) {
-							sendMessage(new TimerDone());
+					}
+					
+					if(ttm.getDisplay().equals("00:00")) {
+						if(user.getUsername().equals(users.get(0).getUsername())){
+							sendMessage(new TimerDone());							
 						}
 					}
 				}
@@ -263,8 +271,10 @@ public class Client extends Thread {
 					}
 				}
 				else if (m instanceof CompanyUpdateMessage) {
-					CompanyUpdateMessage cum = (CompanyUpdateMessage)m; // hehe, cum...
-					((TimelapsePanel)gameFrame.getCurrentPanel()).appendNotification(cum.getMessage());
+					if(gameFrame.getCurrentPanel() instanceof TimelapsePanel){
+						CompanyUpdateMessage cum = (CompanyUpdateMessage)m; // hehe, cum...
+						((TimelapsePanel)gameFrame.getCurrentPanel()).appendNotification(cum.getMessage());
+					}
 				}
 				else if (m instanceof BuyMessage) {
 					System.out.println("buy message");
